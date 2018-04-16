1e1cfdba04b4a5e2dc73eabb00701226cabb9dfa
josecarlosdxu <josecarlosdxu@gmail.com>
2018-04-16 08:29:01 +0200

    add web folder

---
+++ web/index.php
@@ -0,0 +1,38 @@
+<!doctype html>
+<html class="no-js" lang="">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="x-ua-compatible" content="ie=edge">
+        <title></title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <link rel="manifest" href="site.webmanifest">
+        <link rel="apple-touch-icon" href="icon.png">
+        <!-- Place favicon.ico in the root directory -->
+
+        <link rel="stylesheet" href="css/main.css">
+    </head>
+    <body>
+        <!--[if lte IE 9]>
+        <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="https://browsehappy.com/">upgrade your browser</a> to improve your experience and security.</p>
+        <![endif]-->
+
+        <!-- Add your site or application content here -->
+
+
+        <p class="mx-2xs my-2xs">0123458<br>1000121</p>
+        <p class="mx-xs my-xs">0123458<br>1000121</p>
+        <p class="mx-sm my-sm">0123458<br>1000121</p>
+        <p class="mx-md my-md">0123458<br>1000121</p>
+        <p class="mx-lg my-lg">0123458<br>1000121</p>
+        <p class="mx-xl my-xl">0123458<br>1000121</p>
+        <p class="mx-2xl my-2xl">0123458<br>1000121</p>
+        <button class="button">button</button>
+
+
+        <!-- <script src="js/vendor/modernizr-3.5.0.min.js"></script>  -->
+        <!--<script src="js/plugins.js"></script>-->
+        <script src="js/main.js"></script>
+    </body>
+</html>
